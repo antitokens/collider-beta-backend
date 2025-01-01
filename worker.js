4f7ace12753a7ae07352a417143f135e26aae13e
sshmatrix <cryptosauron@protonmail.com>
2025-01-01 16:21:04 +0530

    Fix start & end times

--- worker.js
+++ worker.js
@@ -8,7 +8,7 @@ const ORIGIN = "https://stage.antitoken.pro"; // "http://localhost:3000" || "htt
 const ANTI_TOKEN_MINT = "HB8KrN7Bb3iLWUPsozp67kS4gxtbA4W5QJX4wKPvpump";
 const PRO_TOKEN_MINT = "CWFa2nxUMf5d1WwKtG9FS9kjUKGwKXWSjH8hFdWspump";
 const KV = Antitoken_Collider_Beta;
-const START_TIME = "2024-12-23T00:00:00Z";
+const START_TIME = "2024-12-22T00:00:00Z";
 const END_TIME = "2025-01-03T00:00:00Z";
 
 const duration =
