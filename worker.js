d3fa16d3977767c58c39dfd49fe2ebdb3dad0d69
sshmatrix <cryptosauron@protonmail.com>
2025-01-05 10:28:23 +0530

    v0.9.2-alpha

--- worker.js
+++ worker.js
@@ -5,11 +5,11 @@ import { Buffer } from "buffer";
 const endpoint =
   "https://greatest-smart-tent.solana-mainnet.quiknode.pro/c61afb9af2756c92f1dc812ac2a5b8b68c0602ff";
 const ORIGIN = "https://app.antitoken.pro";
-const ANTI_TOKEN_MINT = "HB8KrN7Bb3iLWUPsozp67kS4gxtbA4W5QJX4wKPvpump";
-const PRO_TOKEN_MINT = "CWFa2nxUMf5d1WwKtG9FS9kjUKGwKXWSjH8hFdWspump";
+const ANTI_TOKEN_MINT = "EWkvvNnLasHCBpeDbitzx9pC8PMX4QSdnMPfxGsFpump";
+const PRO_TOKEN_MINT = "FGWJcZQ3ex8TRPC127NsQBpoXhJXeL2FFpRdKFjRpump";
 const KV = Antitoken_Collider_Alpha;
-const START_TIME = "2024-12-24T00:00:00Z";
-const END_TIME = "2025-01-04T00:00:00Z";
+const START_TIME = "2025-01-06T00:00:00Z";
+const END_TIME = "2025-01-09T00:00:00Z";
 
 const duration =
   Math.round(
