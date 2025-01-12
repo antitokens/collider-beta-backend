15296f9c3eb4da3d1d7eee1c439a2388abb273c8
sshmatrix <cryptosauron@protonmail.com>
2025-01-12 19:46:25 +0530

    Multi-origins: 2

--- worker.js
+++ worker.js
@@ -2,14 +2,18 @@ import { Connection, PublicKey } from "@solana/web3.js";
 
 const endpoint =
   "https://greatest-smart-tent.solana-mainnet.quiknode.pro/c61afb9af2756c92f1dc812ac2a5b8b68c0602ff";
-const ORIGINS = ["https://app.antitoken.pro", "https://lite.antitoken.pro"];
+const ORIGINS = [
+  "https://stage.antitoken.pro",
+  "https://lite.antitoken.pro",
+  //"http://localhost:3000",
+];
 const ANTI_TOKEN_MINT = "EWkvvNnLasHCBpeDbitzx9pC8PMX4QSdnMPfxGsFpump";
 const PRO_TOKEN_MINT = "FGWJcZQ3ex8TRPC127NsQBpoXhJXeL2FFpRdKFjRpump";
-const KV = Antitoken_Collider_Alpha;
+const KV = Antitoken_Collider_Beta;
 
 // Set duration
-const START_TIME = "2025-01-06T00:00:00.000Z";
-const END_TIME = "2025-01-12T00:00:00.000Z";
+const START_TIME = "2025-01-08T00:00:00.000Z";
+const END_TIME = "2025-01-13T00:00:00.000Z";
 
 // Calculate globals
 const startTime = new Date(START_TIME);
