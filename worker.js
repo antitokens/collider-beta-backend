bf787d73cdf0482690830bcbf6c3839adcb70e9d
sshmatrix <cryptosauron@protonmail.com>
2025-01-27 01:12:15 +0530

    Migrate

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ import { compressMetadata } from "./compress";
 const ORIGINS = ["https://poll.antitoken.pro"];
 const ANTI_TOKEN_MINT = "HB8KrN7Bb3iLWUPsozp67kS4gxtbA4W5QJX4wKPvpump";
 const PRO_TOKEN_MINT = "CWFa2nxUMf5d1WwKtG9FS9kjUKGwKXWSjH8hFdWspump";
-const KV = Antitoken_Collider_Poll;
+const KV = Polls;
 
 // Constants
 const metadataInit = {
